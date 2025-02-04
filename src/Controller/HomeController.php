3f54c7a399bba610a451c5fac01cedb96c89efdc
Mathieu <mathieu.barbey18@gmail.com>
2025-02-04 15:03:08 +0100

    display highlighted products on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+
+        $highlightedProducts = $productRepository->findAllHighlightedProducts();
+
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
             'user' => $this->getUser(),
+            'highlightedProducts'=> $highlightedProducts
         ]);
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -16,28 +16,12 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-//    /**
-//     * @return Product[] Returns an array of Product objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Product
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findAllHighlightedProducts(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.highlighted = :highlighted')
+            ->setParameter('highlighted', true) // true au lieu de 1 pour plus de clarté
+            ->getQuery()
+            ->getResult();
+    }
 }
