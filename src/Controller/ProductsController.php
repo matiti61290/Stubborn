2f8966ee6bc135661c1e4a83f11ad4e2ab9090fb
Mathieu <mathieu.barbey18@gmail.com>
2025-02-04 15:34:12 +0100

    product details page created

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -6,11 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Product;
+use App\Entity\Size;
 
 final class ProductsController extends AbstractController
 {
-    #[Route('/sweatshirts', name: 'app_sweatshirts')]
-    public function index(ProductRepository $productRepository): Response
+    #[Route('/sweatshirts', name: 'products')]
+    public function productsAll(ProductRepository $productRepository): Response
     {
 
         $products = $productRepository->findAllProducts();
@@ -18,6 +21,16 @@ final class ProductsController extends AbstractController
         return $this->render('products/productsList.html.twig', [
             'controller_name' => 'ProductsController',
             'products' => $products,
+        ]);
+    }
+
+    #[Route('/sweatshirts/{id}', name:'product_detail')]
+    public function productDetails(?Product $product, EntityManagerInterface $em): Response
+    {
+        $sizes = $em->getRepository(Size::class)->findAll();
+        return $this->render('products/product.html.twig',[
+            'product' => $product,
+            'sizes'=> $sizes
         ]);
     }
 }
